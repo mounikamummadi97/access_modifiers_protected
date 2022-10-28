247896a5ccd92d7e0e07a6a492abed81eab5edf2
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-28 09:59:12 +0530

    first commit

---
+++ access modifier protected/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace protectedAccessModifier
+{
+    class x
+    {
+        protected int X;
+        public x()
+        {
+            X = 20;
+        }
+    }
+    class Y : x
+    {
+        public int getX()
+        {
+            return X;   
+        }
+    }
+   
+    class program
+    {
+        static void Main(string[] args)
+
+        {
+            x obj1 = new x();
+            Y obj2 = new Y();
+            Console.WriteLine("value of x is : {0}", obj2.getX());
+            //Console.WriteLine("value of Y is : {0}", obj1.getY());
+        }
+    }
+}
